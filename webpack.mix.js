7d6e2b4c3448ca1a1f059696453aa48c0a5abc0b
eduardo de la hoz <eduardo.ing.udp@gmail.com>
2020-05-09 01:13:25 -0400

    webpack config
    Co-Authored-By: eduardo de la hoz <xxeduhxx@users.noreply.github.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,32 @@
 const mix = require('laravel-mix');
 
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+|--------------------------------------------------------------------------
+| Mix Asset Management
+|--------------------------------------------------------------------------
+|
+| Mix provides a clean, fluent API for defining some Webpack build steps
+| for your Laravel application. By default, we are compiling the Sass
+| file for the application as well as bundling up all the JS files.
+|
+*/
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+.sass('resources/sass/app.scss', 'public/css');
+
+/**
+* @author Eduardo de la Hoz
+* @description Sirve para sincronizar la vista entre diferentes
+* navegadores abiertos en el mismo pc.
+*/
+mix.browserSync({
+  proxy: 'http://localhost:3000',
+  open: true,
+});
+
+/**
+* @author Eduardo de la Hoz
+* @description Desactivar notificaciones
+* de browserSync.
+*/
+mix.disableNotifications();
